9c0e36d976b32e5c1f78b9d2031a807152fcec4a
Yash Sahu <itsyashsahu@gmail.com>
2023-06-20 00:54:21 +0530

    Updated the husky Pre-commit Script again

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      Jis ke
+      Updated the Commit Script
     </main>
   );
 }
